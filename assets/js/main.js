31ca72d67bd7dafadc5a305b17456520399c64ac
Galih Muhammad <galih0muhammad@gmail.com>
2018-07-13 22:30:27 +0700

    Add tab for display

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,39 @@ function SelectText(element) {
       }
   }
 }
+
+$(document).ready(function(){    
+    $(".fil-cat").click(function() {
+      
+      $('.rk-menu__item').removeClass('active');
+      var parent = $(this).parent();
+      parent.addClass( 'active' );
+
+      var selectedClass = "";
+      
+      selectedClass = $(this).attr("data-rel"); 
+
+      $(".rk-portfolio__items").fadeTo(100, 0.1);
+
+      var items, filtered, query, diff;      
+      items = $(".rk-portfolio__items a.rk-item");
+
+      if (selectedClass === 'all') {
+        filtered  = items;
+        diff      = [];
+      } else {
+        query     = [ '[data-cat="', selectedClass, '"]' ].join('');
+        filtered  = items.filter(query);
+        diff      = items.not(filtered).get();
+      }
+
+      setTimeout(function() {
+
+        $(diff).fadeOut();
+        $(filtered).fadeIn();
+        $(".rk-portfolio__items").fadeTo(300, 1);
+
+      }, 300); 
+    
+    });
+}); 
